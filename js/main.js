cd4efd6d4298d518e76d1887ed5529f89279eb87
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-19 16:35:27 -0300

    Colocoando dentro de uma função.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@ const subtrair =  document.querySelector('#subtrair');
 const somar =  document.querySelector('#somar');
 const braco =  document.querySelector('#braco');
 
+const controle = document.querySelectorAll('.controle-ajuste');
+
 subtrair.addEventListener('click', ( ) => {
-    braco.value = parseInt(braco.value) - 1;
+    manipulaDados(subtrair);
 })
 
 somar.addEventListener('click', ( ) => {
-    braco.value = parseInt(braco.value) + 1;
+    manipulaDados(somar);
 })
+
+function manipulaDados(operacao) {
+    if (operacao === 'subtrair') {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
+}
