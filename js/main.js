74ac7682fe16a80e2ff833793308046a420e4a47
swtrmdwyu <swtrmdwyu@gmail.com>
2023-09-19 16:50:44 -0300

    Botões funcionais e dinâmicos.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-const subtrair =  document.querySelector('#subtrair');
-const somar =  document.querySelector('#somar');
-const braco =  document.querySelector('#braco');
-
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento) => {
-        calculo(evento.target.textContent)
+        calculo(evento.target.parentNode, evento.target.textContent)
     })
 })
 
-function calculo(operacao) {
+function calculo(controle, operacao) {
+
+    const peca = controle.querySelector('.controle-contador')
+
     if (operacao === '-') {
         braco.value = parseInt(braco.value) - 1;
     } else {
